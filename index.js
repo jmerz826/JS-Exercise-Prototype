9eb37469e6c38a4690705958c017a9e14c7b94fb
John Merz <jmerz826@gmail.com>
2021-10-13 16:08:29 -0400

    Person constructor complete. Person is eating and pooping with ease.

--- index.js
+++ index.js
@@ -39,11 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.stomach = [];
+  this.age = age;
+  this.name = name;
+  
   
 }
 
+Person.prototype.eat = function (food) {
+  if (this.stomach.length !== 10) {
+    this.stomach.push(food);
+  }
+};
 
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+}
 
 
 
